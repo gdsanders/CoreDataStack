d1936f1b702f76ec1149f9a8ad26a95baca28fb7
gdsanders <gd@digitalequity.com>
2015-10-06 09:08:17 -0500

    CDHelper class

--- CoreDataStack/CDHelper.swift
+++ CoreDataStack/CDHelper.swift
@@ -1,14 +1,15 @@
 //
-//  CDHelpr.swift
+//  CDHelper.swift
 //  CoreDataStack
 //
-//  Created by G.D. Sanders on 10/5/15.
+//  Created by G.D. Sanders on 10/6/15.
 //  Copyright © 2015 DigitalEquity, Inc. All rights reserved.
 //
 
 import Foundation
 import CoreData
 
-class CDHelper {
+class CDHelper  {
+    
     
 }
